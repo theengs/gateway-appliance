59cfdbf892338a4cedb84afde7968a16b8b82973
Koen Vervloesem <koen@vervloesem.eu>
2022-07-25 13:19:36 +0200

    Fix shellscripts and use sudo for snap

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -9,24 +9,24 @@ rm -rf "$build_dir"
 mkdir "$build_dir"
 
 for model_assertion in *.model; do
-    echo "Building image for model assertion $model_assertion..."
-    ubuntu-image snap "$model_assertion" --output-dir="$build_dir" --validation=enforce
-    if [[ -f "$build_dir/pi.img" ]]; then
-        image_file=pi.img
-    elif [[ -f "$build_dir/pc.img" ]]; then
-        image_file=pc.img
-    else
-        echo "No suitable image file found"
-        exit 1
-    fi
+	echo "Building image for model assertion $model_assertion..."
+	ubuntu-image snap "$model_assertion" --output-dir="$build_dir" --validation=enforce
+	if [[ -f "$build_dir/pi.img" ]]; then
+		image_file=pi.img
+	elif [[ -f "$build_dir/pc.img" ]]; then
+		image_file=pc.img
+	else
+		echo "No suitable image file found"
+		exit 1
+	fi
 
-    compressed_image_file="$build_dir/${model_assertion%.model}.img.xz"
-    manifest_file="$build_dir/${model_assertion%.model}.manifest"
+	compressed_image_file="$build_dir/${model_assertion%.model}.img.xz"
+	manifest_file="$build_dir/${model_assertion%.model}.manifest"
 
-    echo "Compressing image to $compressed_image_file..."
-    xz -9 "$build_dir/$image_file"
-    mv "$build_dir/$image_file.xz" "$compressed_image_file"
+	echo "Compressing image to $compressed_image_file..."
+	xz -9 "$build_dir/$image_file"
+	mv "$build_dir/$image_file.xz" "$compressed_image_file"
 
-    echo "Saving seed manifest to $manifest_file"
-    mv "$build_dir/seed.manifest" "$manifest_file"
+	echo "Saving seed manifest to $manifest_file"
+	mv "$build_dir/seed.manifest" "$manifest_file"
 done
--- scripts/sign-model-assertions.sh
+++ scripts/sign-model-assertions.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [ $# -eq 0 ]; then
-    echo "Specify your snapcraft key to sign the assertion model with"
-    exit 1
+	echo "Specify your snapcraft key to sign the assertion model with"
+	exit 1
 fi
 
 signing_key=$1
 
 for model_assertion in *.json; do
-    snap sign -k "$signing_key" < "$model_assertion" > "${model_assertion%.json}.model"
+	snap sign -k "$signing_key" <"$model_assertion" >"${model_assertion%.json}.model"
 done
